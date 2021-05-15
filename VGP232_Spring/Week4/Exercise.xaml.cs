38ade2f3eab11baac7daea0b08558629381191eb
jack Zhao <a799949591@gmail.com>
2021-05-14 17:26:01 -0700

    Week_5

---
+++ VGP232_Spring/Week4/Exercise.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Week4
+{
+    /// <summary>
+    /// Interaction logic for Exercise.xaml
+    /// </summary>
+    public partial class Exercise : Window
+    {
+        private int counter = 0;
+        public Exercise()
+        {
+            InitializeComponent();
+        }
+
+        private void IncrementCount(object sender, RoutedEventArgs e)
+        {
+            ++counter;
+            tbCounter.Text = string.Format($"Count = {counter}");
+        }
+
+        private void HelloMessage(object sender, RoutedEventArgs e)
+        {
+            tbTitle.Text = string.Format($"Hello {tboxUserName.Text} !");
+        }
+    }
+}
---
+++ VGP232_Spring/Week_5_Console_Example/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace Week_5_Console_Example
+{
+    class Program
+    {
+        public static byte[] Compress(byte[] raw)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (GZipStream gZip = new GZipStream(memory,CompressionMode.Compress, true))
+                {
+                    gZip.Write(raw, 0, raw.Length);
+                }
+                return memory.ToArray();
+            }
+        }
+        public static byte[] Decompress(byte[] compressed, int originalSize)
+        {
+            byte[] uncompressed = new byte[originalSize];
+            using (MemoryStream memory = new MemoryStream(compressed))
+            {
+                using (GZipStream gZip = new GZipStream(memory, CompressionMode.Decompress, true))
+                {
+                    gZip.Read(uncompressed, 0, originalSize);
+                }
+                return uncompressed;
+            }
+        }
+
+        public static void ZipArchiveExample()
+        {
+            string myDoc = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string ImagesPath = Path.Combine(myDoc, "Images");
+            string ImagesZip = Path.Combine(ImagesPath, "Image.zip");
+
+            DirectoryInfo dir = new DirectoryInfo(ImagesPath);
+            FileInfo[] allImagesFiles = dir.GetFiles("*.png");
+            using (FileStream file = new FileStream(ImagesZip, FileMode.Create))
+            {
+                using (ZipArchive zip = new ZipArchive(file, ZipArchiveMode.Update))
+                {
+                    foreach (var png in allImagesFiles)
+                    {
+                        ZipArchiveEntry entry = zip.CreateEntryFromFile(png.FullName, png.Name, CompressionLevel.Optimal);
+                    }
+                }
+            }
+        }
+
+        public static void ZipArchiveExtractionExample()
+        {
+            string myDoc = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string ImagesPath = Path.Combine(myDoc, "Images");
+            string ImagesZip = Path.Combine(ImagesPath, "Image.zip");
+
+            string extractedImagesPath = Path.Combine(myDoc, "Images/ExtractedImages");
+
+            if (Directory.Exists(extractedImagesPath))
+            {
+                Directory.Delete(extractedImagesPath, true);
+            }
+
+            ZipFile.ExtractToDirectory(ImagesZip, extractedImagesPath);
+        }
+        static void Main(string[] args)
+        {
+            //byte[] text = Encoding.ASCII.GetBytes(new string('a',200000));
+            //File.WriteAllBytes("raw.bin", text);
+
+            //byte[] compressed = Compress(text);
+            //File.WriteAllBytes("compressed.bin", compressed);
+
+            //byte[] expand = Decompress(compressed, 200000);
+            //File.WriteAllBytes("expand.bin", expand);
+
+            ZipArchiveExtractionExample();
+        }
+    }
+}
